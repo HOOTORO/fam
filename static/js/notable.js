5dc7b0d4504a14f159b67db9c442191435790740
Hootsman <marukubaka@gmail.com>
2023-07-15 20:58:31 +0300

    add content

--- static/js/notable.js
+++ static/js/notable.js
@@ -12,18 +12,20 @@ function draw(arr) {
         app.appendChild(par);
     }
 }
-const tempstr = `<strong>$1</strong> <br> <em>Birth</em>: $2<br>$3`;
+const tempstr = `<strong>$1</strong><br>$4<br> <em>Birth</em>: $2<br>$3`;
 const raw = dd.map((x) => {
     if (x.death) {
         return tempstr
             .replace("$1", x.name)
             .replace("$2", x.birth)
-            .replace("$3", `Death: ${x.death}`);
+            .replace("$3", `Death: ${x.death}`)
+            .replace("$4", `Relationship: ${x.rel}`);
     } else {
         return tempstr
             .replace("$1", x.name)
             .replace("$2", x.birth)
-            .replace("$3", ``);
+            .replace("$3", ``)
+            .replace("$4", `Relationship: ${x.rel}`);
     }
 });
 
